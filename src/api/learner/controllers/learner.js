53077cb2bbe1f7f8b21a0f7ebb305d003f433536
Phaks323@gmail.com <phaks323@gmail.com@gmail.com>
2024-06-06 12:34:11 +0200

    gvg

---
+++ src/api/learner/controllers/learner.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * learner controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::learner.learner');
